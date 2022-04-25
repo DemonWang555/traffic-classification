09b2cc931000b12a474d325ca86fcb6efb2f943c
wpcbupt <57081991+wpcbupt@users.noreply.github.com>
2022-04-25 22:11:36 +0800

    Update traffic_classification.py

--- 深度学习部署/traffic/app/traffic_classification.py
+++ 深度学习部署/traffic/app/traffic_classification.py
@@ -39,42 +39,6 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # s.bind((conf['socket_server']['host'], conf.getint('socket_server', 'port')))
 s.bind(('', conf.getint('socket_server', 'port')))
 s.listen(128)
-
- 
-def make_print_to_file(path='./'):
-    '''
-    path， it is a path for save your log about fuction print
-    example:
-    use  make_print_to_file()   and the   all the information of funtion print , will be write in to a log file
-    :return:
-    '''
-    import sys
-    import os
-    import sys
-    import datetime
- 
-    class Logger(object):
-        def __init__(self, filename="Default.log", path="./"):
-            self.terminal = sys.stdout
-            self.log = open(os.path.join(path, filename), "a", encoding='utf8',)
- 
-        def write(self, message):
-            self.terminal.write(message)
-            self.log.write(message)
- 
-        def flush(self):
-            pass
- 
- 
- 
- 
-    fileName = datetime.datetime.now().strftime('day'+'%Y_%m_%d')
-    sys.stdout = Logger(fileName + '.log', path=path)
- 
-    #############################################################
-    # 这里输出之后的所有的输出的print 内容即将写入日志
-    #############################################################
-    print(fileName.center(60,'*'))
  
 
 # 将检测结果存入数据库
@@ -239,5 +203,4 @@ def main():
 
 
 if __name__ == '__main__':
-    make_print_to_file(path='./')
     main()
